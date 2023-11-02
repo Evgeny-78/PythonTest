243fe22e84bb27012ead8edc612f0f3ce6ad3c85
Evgeny-78 <evgebiu_spb_92@rambler.ru>
2023-11-02 16:54:18 +0300

    next commit

--- bibl_class_test.py
+++ bibl_class_test.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Integer:
     'Дескриптор для значений типа Integer'
     def __set_name__(self, owner, name):
@@ -10,7 +13,19 @@ class Integer:
         if isinstance(value, int):
             instance.__dict__[self.name] = value
         else:
-            raise ValueError('Ошибка значения. Значение должно быть Int')
+            raise ValueError(f'Ошибка значения. Значение {self.name} должно быть Int')
+
+class ListClass(Integer):
+    'Дескриптор для значений типа List'
+    def __set__(self, instance, value):
+        if isinstance(value, list) and not hasattr(instance, self.name):
+            instance.__dict__[self.name] = value
+        # elif isinstance(value, list) and hasattr(instance, self.name):
+        #     raise Warning('Родословную нельзя изменить после назначения')
+        elif not isinstance(value, list):
+            raise ValueError(f'Ошибка значения. Значение {self.name} должно быть List')
+        else:
+            raise Warning('Родословную нельзя изменить после назначения')
 
 class String(Integer):
     'Дескриптор для значений типа String'
@@ -18,17 +33,27 @@ class String(Integer):
         if isinstance(value, str):
             instance.__dict__[self.name] = value
         else:
-            raise ValueError('Ошибка значения. Значение должно быть Str')
+            raise ValueError(f'Ошибка значения. Значение {self.name} должно быть Str')
+
+class Float(Integer):
+    'Дескриптор для значений типа Float'
+    def __set__(self, instance, value):
+        if isinstance(value, float):
+            instance.__dict__[self.name] = value
+        else:
+            raise ValueError(f'Ошибка значения. Значение {self.name} должно быть Float')
 
 class Animals:
 
     _listObj = []      # список объектов класса
     _number = 0
+    _iter_index = 0
     age = Integer()
-    weight = Integer()
+    weight = Float()
     name = String()
     height = Integer()
-    _iter_index = 0
+    sex = String()
+    rod = ListClass()
 
     def __str__(self):
         return f'Животное по кличке: {self.name} - весом: {self.weight} кг - возрастом: {self.age} лет - высотой: {self.height} см'
@@ -39,11 +64,13 @@ class Animals:
         cls._listObj.append(self)
         return self
 
-    def __init__(self, name: str, age: int, weight: int, height: int):
+    def __init__(self, name: str, age: int, weight: float, height: int, sex: str, rod: list):
         self.age = age
         self.weight = weight
         self.name = name
         self.height = height
+        self.sex = sex
+        self.rod = rod
 
     def __getattribute__(self, item):
         return object.__getattribute__(self, item)
@@ -93,8 +120,8 @@ class Cat(Animals):
     def __str__(self):
         return f'Киса по кличке: {self.name} - весом: {self.weight} кг - возрастом: {self.age} лет - высотой: {self.height} см'
 
-    def __init__(self, name: str, breed: str, age: int, weight: int, height: int):
-        super().__init__(name, age, weight, height)
+    def __init__(self, name: str, breed: str, age: int, weight: float, height: int, sex: str, rod: list):
+        super().__init__(name, age, weight, height, sex, rod)
         self.breed = breed
 
     def action_mymy(self):
@@ -102,6 +129,33 @@ class Cat(Animals):
 
     def action_myrmyr(self):
         print(f'Киса {self.name} мурлыкает')
+
+    def __add__(self, other):
+        if isinstance(other, Cat):
+            if self.sex != other.sex:
+                rod = self.rod + other.rod + [self.name, other.name]
+                breed = self.breed if self.sex == 'мужской' else other.breed
+                name_cat = input('Введите имя котёнка: ')
+                return Cat(name_cat, breed, 0, 0.5,
+                       int((self.height + other.height) / 4), random.choice(['мужской', 'женский']), rod)
+            else:
+                raise ValueError('Пол котиков должен быть разным')
+        elif isinstance(other, Corm):
+            if other.weight > 0:
+                self.weight += 0.1
+                other.weight -= 0.1
+            else:
+                raise ValueError('Корм закончился')
+        else:
+            raise ArithmeticError("Правый операнд должен быть типом Cat или Corm")
+
+    def __eq__(self, other):
+        if (self.sex == other.sex and -2 <= self.weight - other.weight <= 2 and -5 <= self.height - other.height <= 5
+                and -1 <= self.age - other.age <= 1 and -2 <= len(self.rod) - len(other.rod) <= 2 and isinstance(other, Cat)):
+            return True
+        else:
+            return False
+
 
 
 class Dog(Animals):
@@ -115,3 +169,17 @@ class Dog(Animals):
 
     def action_wooo(self):
         print(f'Собака {self.name} воет')
+
+class Corm:
+    type_corm = String()
+    animal = String()
+    weight = Float()
+    def __str__(self):
+        return f'{self.type_corm} корм {self.animal}, {self.weight} кг'
+
+    def __init__(self, animal: str, type_corm: str, weight: float):
+        self.animal = animal
+        self.type_corm = type_corm
+        self.weight = weight
+
+
